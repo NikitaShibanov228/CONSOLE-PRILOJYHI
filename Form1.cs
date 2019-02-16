1660a6dc93d874a17347ca618c2c40c3d5439eb3
NikitaShibanov228 <47685880+NikitaShibanov228@users.noreply.github.com>
2019-02-16 10:30:26 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Tutorial.SqlConn;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            zzz();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void zzz()
+        {
+            int age = 23;
+            string sqlExpression = "Delete FROM test WHERE Qeq = 4444";
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+                MySqlCommand command = new MySqlCommand(sqlExpression, conn);
+                int number = command.ExecuteNonQuery();
+                
+                textBox1.Text = "Удалено объектов: {0}" + number;
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Progs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorial.SqlConn;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+
+namespace WindowsFormsApp6
+{
+    class Program1
+    {
+        public static void Main1()
+        {
+            int age = 23;
+            string sqlExpression = "Delete FROM test WHERE Qeq = 4444";
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+                MySqlCommand command = new MySqlCommand(sqlExpression, conn);
+                int number = command.ExecuteNonQuery();
+                Console.WriteLine("Удалено объектов: {0}", number);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+
+}
